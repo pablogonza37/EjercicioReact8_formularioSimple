3c047ad36a46817adfaef3969b718f5d53c5c8fa
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-29 01:55:14 -0300

    se creo el componente TituloFormulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import TituloFormulario from "./components/TituloFormulario";
 
 function App() {
-
   return (
     <>
-    
-      
+      <Container>
+        <TituloFormulario></TituloFormulario>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TituloFormulario.jsx
@@ -0,0 +1,7 @@
+const TituloFormulario = () => {
+  return <section>
+    <h1>Formulario simple</h1>
+  </section>;
+};
+
+export default TituloFormulario;
